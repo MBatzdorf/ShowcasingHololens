88589cb0f244a72284a311d3131727b5ae296714
Unknown <alexfrummet@online.de>
2017-09-19 22:27:41 +0200

    Fixed some minor stuff
    - ManipulationType can't be determined by other object menus now

--- Assets/Scripts/ToggleManipulationType.cs
+++ Assets/Scripts/ToggleManipulationType.cs
@@ -33,31 +33,40 @@ public class ToggleManipulationType : MonoBehaviour {
 
     private void ChangeManipulationType(ManipulationMode manipulationMode)
     {
-        SpatialManipulator spatialManipulator = GetManipulator();
-        if (spatialManipulator == null)
+        if (gameObject == ObjectStateManager.Instance.SelectedObject)
         {
-            return;
-        }
-        if (spatialManipulator.Mode != manipulationMode)
-        {
-            spatialManipulator.Mode = manipulationMode;
-            Debug.Log("NEW manipulation mode: " + spatialManipulator.Mode);
+            SpatialManipulator spatialManipulator = GetManipulator();
+            if (spatialManipulator == null)
+            {
+                return;
+            }
+            if (spatialManipulator.Mode != manipulationMode)
+            {
+                spatialManipulator.Mode = manipulationMode;
+                Debug.Log("NEW manipulation mode: " + spatialManipulator.Mode);
 
+            }
+        }
+        else
+        {
+            Debug.Log("This object is currently not selected!");
         }
     }
 
     private SpatialManipulator GetManipulator()
     {
-        var lastSelectedObject = ObjectStateManager.Instance.SelectedObject;
-        if (lastSelectedObject == null)
+        //var lastSelectedObject = ObjectStateManager.Instance.SelectedObject;
+        /*if (lastSelectedObject == null)
         {
             Debug.Log("No selected element found");
             return null;
-        }
-        var manipulator = lastSelectedObject.GetComponent<SpatialManipulator>();
+        }*/
+        //var manipulator = lastSelectedObject.GetComponent<SpatialManipulator>();
+        SpatialManipulator manipulator = GetComponent<SpatialManipulator>();
         if (manipulator == null)
         {
-            manipulator = lastSelectedObject.GetComponentInChildren<SpatialManipulator>();
+            //manipulator = lastSelectedObject.GetComponentInChildren<SpatialManipulator>();
+            manipulator = GetComponentInChildren<SpatialManipulator>();
         }
 
         if (manipulator == null)
