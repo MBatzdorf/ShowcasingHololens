571d595b985bc1a4c14a30bc9fa002acce4a0e50
Marco Batzdorf <mbatzdorf@yahoo.de>
2017-09-21 13:43:41 +0200

    COntext menu only visible if object active

---
+++ Assets/Scripts/UI/ContextMenuBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextMenuBehavior : MonoBehaviour {
+
+	public GameObject associatedObject;
+	private ObjectStateManager stateManager;
+	private Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+		stateManager = ObjectStateManager.Instance;
+		canvas = GetComponent<Canvas> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (associatedObject == stateManager.SelectedObject) {
+			canvas.enabled = true;
+		} else {
+			canvas.enabled = false;
+		}
+	}
+}
--- Assets/Scripts/UI/InteractionTypeButtonBehavior.cs
+++ Assets/Scripts/UI/InteractionTypeButtonBehavior.cs
@@ -7,6 +7,7 @@ public class InteractionTypeButtonBehavior : MonoBehaviour {
 
 	public Button button;
 	public ToggleManipulationType manipulationToggler;
+	public SpatialManipulator manipulator;
 	public Image manipulateButtonImage;
 	public Sprite rotateImage;
 	public Sprite scaleImage;
@@ -14,6 +15,7 @@ public class InteractionTypeButtonBehavior : MonoBehaviour {
 
 	private enum States {rotate, move, scale, numberOfTypes};
 	private States currentState;
+	private ManipulationMode lastMode;
 
 
 	// Use this for initialization
@@ -22,21 +24,42 @@ public class InteractionTypeButtonBehavior : MonoBehaviour {
 		currentState = States.rotate;
 	}
 
+	void Update()
+	{
+		Debug.Log ("Current Mode: " + manipulator.Mode);
+		if (manipulator.Mode != lastMode) {
+			switch (manipulator.Mode) {
+			case ManipulationMode.MOVEMENT:
+				manipulateButtonImage.sprite = moveImage;
+				break;
+			case ManipulationMode.ROTATION:
+				manipulateButtonImage.sprite = rotateImage;
+				break;
+			case ManipulationMode.SCALING:
+				manipulateButtonImage.sprite = scaleImage;
+				break;
+			}
+		}
+
+		lastMode = manipulator.Mode;
+	}
+
 	void OnButtonClicked()
 	{
+		Debug.Log ("Button clicked!");
 		currentState += 1;
 		switch (currentState) {
 		case States.move:
-			manipulateButtonImage.sprite = moveImage;
+			Debug.Log ("Toggled Move");
 			manipulationToggler.OnMove ();
 			break;
 		case States.scale:
-			manipulateButtonImage.sprite = scaleImage;
+			Debug.Log ("Toggled Scale");
 			manipulationToggler.OnScale ();
 			break;
 		case States.numberOfTypes:
 			currentState = 0;
-			manipulateButtonImage.sprite = rotateImage;
+			Debug.Log ("Toggled Rotate");
 			manipulationToggler.OnRotate ();
 			break;
 		}
