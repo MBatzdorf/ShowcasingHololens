4f06805f89be9881e907b00fb7039d1907ba1c4b
Marco Batzdorf <mbatzdorf@yahoo.de>
2017-09-21 12:41:19 +0200

    Toggle script for toggle button added

---
+++ Assets/Scripts/UI/InteractionTypeButtonBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionTypeButtonBehavior : MonoBehaviour {
+
+	public Button button;
+	public ToggleManipulationType manipulationToggler;
+	public Image manipulateButtonImage;
+	public Sprite rotateImage;
+	public Sprite scaleImage;
+	public Sprite moveImage;
+
+	private enum States {rotate, move, scale, numberOfTypes};
+	private States currentState;
+
+
+	// Use this for initialization
+	void Start () {
+		button.onClick.AddListener (OnButtonClicked);
+		currentState = States.rotate;
+	}
+
+	void OnButtonClicked()
+	{
+		currentState += 1;
+		switch (currentState) {
+		case States.move:
+			manipulateButtonImage.sprite = moveImage;
+			manipulationToggler.OnMove ();
+			break;
+		case States.scale:
+			manipulateButtonImage.sprite = scaleImage;
+			manipulationToggler.OnScale ();
+			break;
+		case States.numberOfTypes:
+			currentState = 0;
+			manipulateButtonImage.sprite = rotateImage;
+			manipulationToggler.OnRotate ();
+			break;
+		}
+	}
+}
