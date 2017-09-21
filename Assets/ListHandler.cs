d362fa1b342fc655430c0351eeffb05fe45a73f0
Unknown <alexfrummet@online.de>
2017-09-21 23:53:58 +0200

    Add "Tutorial" screen and hand guidance
    - tutorial screen can be selected via dropdown menu
    - hand guidance still needs a gameobject that indicates when hands are leaving (e.g. image)

---
+++ Assets/ListHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListHandler : MonoBehaviour {
+
+    public GameObject tutorial;
+    private const int TUTORIAL = 3;
+    public enum MenuOptions
+    {
+        LEHRBUCH,
+        TUTORIAL
+    }
+    private Dropdown dropdownMenu;
+    // Use this for initialization
+	void Start () {
+        dropdownMenu = GetComponent<Dropdown>();
+    }
+	
+    public void OnOptionChanged()
+    {
+        if(dropdownMenu.value == TUTORIAL)
+        {
+            tutorial.SetActive(true);
+        }
+    }
+
+    public void OnSetDefaultMenuOption()
+    {
+        dropdownMenu.value = 0;
+    }
+}
